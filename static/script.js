11947f79017c9f94c900b90f4d77ddba393db277
Henrique Schell <henrique.schell@gmail.com>
2025-08-26 00:51:45 -0300

    update

--- static/script.js
+++ static/script.js
@@ -1,5 +1,8 @@
 const btnMenu = document.getElementById('btn_hamburguer')
+const navMobile = document.getElementById('navMobile')
 
 btnMenu.addEventListener('click', () => {
     btnMenu.classList.toggle('ativar')
+    
+    navMobile.classList.toggle('ativo')
 })
