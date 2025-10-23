b2cdf382b14e0542e2b25a72cf118d537dea8652
Henrique Schell <henrique.schell@gmail.com>
2025-10-22 22:31:28 -0300

    update

--- static/script.js
+++ static/script.js
@@ -1,8 +1,11 @@
 const btnMenu = document.getElementById('btn_hamburguer')
 const navMobile = document.getElementById('navMobile')
+const btnNavbarMobile = document.getElementById('btnNavbarMobile')
+
+const card1 = document.getElementById('card1')
+const card_view = document.getElementById('card_view')
 
 btnMenu.addEventListener('click', () => {
-    //btnMenu.classList.toggle('ativar')
     
     if (navMobile.style.display == 'none'){
         navMobile.style.display = 'flex'
@@ -10,5 +13,20 @@ btnMenu.addEventListener('click', () => {
     }else{
         navMobile.style.display = 'none'
         btnMenu.classList.remove('ativar')
+    }
+})
+
+btnNavbarMobile.addEventListener('click', () => {
+    if (navMobile.style.display == 'flex'){
+        navMobile.style.display = 'none'
+        btnMenu.classList.remove('ativar')
+    }
+})
+
+card1.addEventListener('click', () => {
+    if (card_view.style.display === '' || card_view.style.display === 'none') {
+        card_view.style.display = 'flex';
+    } else {
+        card_view.style.display = 'none';
     }
 })
